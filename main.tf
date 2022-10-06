1c1133706d4ef4f37336c39ec9b1db88445f6c4f
Trevor Curtis <trevorscurtis@somaradio.ca>
2022-10-06 10:45:33 -0400

    Adding Ansible playbook.

--- main.tf
+++ main.tf
@@ -1,41 +1,55 @@
 terraform {
   required_providers {
     linode = {
-      source = "linode/linode"
+      source  = "linode/linode"
       version = "1.27.1"
     }
   }
 }
 
 variable "root-password" {
-    description = "Root user's password"
-    type = string
-    sensitive = true
+  description = "Root user's password"
+  type        = string
+  sensitive   = true
 }
 
 variable "linode-token" {
-    description = "Linode API key"
-    type = string
-    sensitive = true
+  description = "Linode API key"
+  type        = string
+  sensitive   = true
 }
 
 variable "ssh-pubkey" {
-    description = "Public SSH key"
-    type = string
-    sensitive = true
+  description = "Public SSH key"
+  type        = string
+  sensitive   = true
 }
 
 provider "linode" {
   token = var.linode-token
 }
 
-
 resource "linode_instance" "workportal-server" {
-        image = "linode/centos-stream9"
-        label = "workportal-server"
-        group = "Terraform-managed"
-        region = "ca-central"
-        type = "g6-standard-1"
-        authorized_keys = [ var.ssh-pubkey ]
-        root_pass = var.root-password
+  image           = "linode/centos-stream9"
+  label           = "workportal-server"
+  group           = "Terraform-managed"
+  region          = "ca-central"
+  type            = "g6-standard-1"
+  authorized_keys = [var.ssh-pubkey]
+  root_pass       = var.root-password
+
+  provisioner "remote-exec" {
+    inline = ["sudo dnf -y install python3-libs"]
+
+    connection {
+      host = "${linode_instance.workportal-server.ip_address}"
+      type        = "ssh"
+      private_key = file("~/.ssh/id_rsa")
+      user        = "root"
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${linode_instance.workportal-server.ip_address},' -u root ../workportal-server/rhel-playbook.yml"
+  }
 }
